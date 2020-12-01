5809f58e7e3b81c7576b1a7d364dcf391cfb6f2e
Owais Islam <owaisislam@gmail.com>
2020-12-01 14:28:57 -0600

    added validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -16,7 +16,6 @@ const pizzaController = {
 				res.status(400).json(err);
 			});
 	},
-
 	// get one pizza by id
 	getPizzaById({ params }, res) {
 		Pizza.findOne({ _id: params.id })
@@ -37,17 +36,18 @@ const pizzaController = {
 				res.status(400).json(err);
 			});
 	},
-
 	// createPizza
 	createPizza({ body }, res) {
 		Pizza.create(body)
 			.then((dbPizzaData) => res.json(dbPizzaData))
 			.catch((err) => res.status(400).json(err));
 	},
-
 	// update pizza by id
 	updatePizza({ params, body }, res) {
-		Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+		Pizza.findOneAndUpdate({ _id: params.id }, body, {
+			new: true,
+			runValidators: true,
+		})
 			.then((dbPizzaData) => {
 				if (!dbPizzaData) {
 					res.status(404).json({ message: "No pizza found with this id!" });
@@ -57,7 +57,6 @@ const pizzaController = {
 			})
 			.catch((err) => res.status(400).json(err));
 	},
-
 	// delete pizza
 	deletePizza({ params }, res) {
 		Pizza.findOneAndDelete({ _id: params.id })
